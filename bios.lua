e9d215be90b67c816528084112424c2a4877c8fa
alekso56 <simple_and_easy@hotmail.com>
2014-09-19 01:01:50 +0200

    1.64pr5

--- bios.lua
+++ bios.lua
@@ -496,8 +496,10 @@ end
 
 -- Install the lua part of the HTTP api (if enabled)
 if http then
+    local nativeHTTPRequest = http.request
+
     local function wrapRequest( _url, _post, _headers )
-        local ok, err = http.request( _url, _post, _headers )
+        local ok, err = nativeHTTPRequest( _url, _post, _headers )
         if ok then
             while true do
                 local event, param1, param2 = os.pullEvent()
@@ -517,6 +519,14 @@ if http then
 
     http.post = function( _url, _post, _headers )
         return wrapRequest( _url, _post or "", _headers )
+    end
+
+    http.request = function( _url, _post, _headers )
+        local ok, err = nativeHTTPRequest( _url, _post, _headers )
+        if not ok then
+            os.queueEvent( "http_failure", _url, err )
+        end
+        return ok, err
     end
 end
 
